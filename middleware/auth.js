7f31201706d6e2d1934390de63eb39e19dc00aca
mostafa <mostafameerzad@gmail.com>
2024-08-08 11:29:30 +0430

    create auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({ msg: "no token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JSt_SECRET);
+    req.user = decoded.user;
+    next();
+    
+  } catch (err) {
+    return res.status(401).json({ msg: "token is not valid" });
+  }
+};
